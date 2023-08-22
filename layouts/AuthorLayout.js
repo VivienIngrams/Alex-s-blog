1026784cd8e10ebaeeb580edfc0cfd29fbd2e0ad
VivienIngrams <vivingrams@gmail.com>
2023-08-22 16:13:11 +0100

    Layout adjustments for mobile

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -14,18 +14,18 @@ export default function AuthorLayout({ children, frontMatter }) {
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
       <div className="bg-neutral-200">
-        <div className="flex-col items-center space-y-2 ">
+        <div className="flex-col items-center">
           <div className=" flex flex-col items-end xl:col-span-2 ">
-            <h3 className=" pt-10 font-playfair text-6xl leading-8 tracking-tight text-black md:pt-40">
+            <h3 className=" pt-10 font-playfair text-5xl leading-8 tracking-tight text-black md:pt-40">
               {name}
             </h3>
           </div>
-          <div className="prose max-w-none pt-2 text-right md:ml-20 md:pl-20 md:pb-10">
+          <div className="prose max-w-none pt-2 text-right font-khand md:ml-20 md:pl-20 md:pb-10">
             <div>{children}</div>{' '}
           </div>
           <div className="flex-grow items-center text-center xl:col-span-2">
             <Link
-              className="m-10 rounded border-2 border-yellow-600 bg-neutral-100 px-4 py-2 font-khand font-bold text-black "
+              className="mx-10 rounded border-2 border-yellow-600 bg-neutral-100 px-4 py-2 font-khand font-bold text-black "
               href="/projects"
             >
               Current Research
